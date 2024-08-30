9ed97b069e0d248d2246458108777df5f90f8ffe
lukas <lmuenter@gfbio.org>
2024-08-30 12:59:47 +0200

    add ability to define custom schemas

--- pelican/plugins/engrave/tests/test_engrave.py
+++ pelican/plugins/engrave/tests/test_engrave.py
@@ -7,6 +7,7 @@ import pytest
 from pelican import Pelican
 from pelican.settings import read_settings
 
+N_PREVIOUS_QRCODES = 5
 
 @pytest.fixture
 def temp_path():
@@ -36,7 +37,7 @@ Test article.
 
 
 @pytest.fixture
-def create_previous_qrcodes(temp_path, num_dummy_files=5):
+def create_previous_qrcodes(temp_path, num_dummy_files=N_PREVIOUS_QRCODES):
     """Create previous qr codes to simulate a previous run."""
     engrave_path = Path(temp_path) / "output" / "engrave"
     engrave_path.mkdir(parents=True, exist_ok=True)
@@ -49,9 +50,8 @@ def test_plugin_functionality(create_previous_qrcodes, create_article, temp_path
     """Test basic plugin functionality: directory cleanup and QRCOde generation."""
     engrave_output_path = Path(temp_path) / "output" / "engrave"
     svg_files = list(engrave_output_path.glob("*.svg"))
-    print(svg_files)
     assert (
-        len(svg_files) == 5
+        len(svg_files) == N_PREVIOUS_QRCODES
     ), "Engrave dir should contain 5 files of a simulated previous run."
 
     settings = read_settings(
@@ -108,3 +108,50 @@ def test_no_siteurl(create_article, temp_path, caplog):
     if engrave_output_path.exists():
         svg_files = list(engrave_output_path.glob("*.svg"))
         assert len(svg_files) == 0, "No QR Code should have been generated."
+
+
+def test_wrong_scheme(create_article, temp_path, caplog):
+    """Test with unsupported URL scheme."""
+    settings = read_settings(
+        override={
+            "SITEURL": "ftp://example.com",
+            "PATH": temp_path,
+            "OUTPUT_PATH": os.path.join(temp_path, "output"),
+            "PLUGIN_PATHS": ["../../"],
+            "PLUGINS": ["engrave"],
+            "ENGRAVE_ALLOWED_SCHEMES": ["https"],
+        }
+    )
+    pelican = Pelican(settings=settings)
+    pelican.run()
+
+    assert (
+        "URL scheme not allowed" in caplog.text
+    ), "Plugin should throw an error for wrong URL schemes."
+
+
+def test_alternative_scheme(temp_path, create_article, caplog):
+    """Test QR code generation with alternative allowed URL scheme."""
+    settings = read_settings(
+        override={
+            "SITEURL": "http://example.com",
+            "PATH": temp_path,
+            "OUTPUT_PATH": os.path.join(temp_path, "output"),
+            "PLUGIN_PATHS": ["../../"],
+            "PLUGINS": ["engrave"],
+            "ENGRAVE_ALLOWED_SCHEMES": ["http", "https"],
+        }
+    )
+    pelican = Pelican(settings=settings)
+    pelican.run()
+
+    engrave_output_path = Path(temp_path) / "output" / "engrave"
+    assert (
+        engrave_output_path.exists()
+    ), "Output dir should be created for allowed URL scheme."
+    svg_files = list(engrave_output_path.glob("*.svg"))
+    assert len(svg_files) == 1, "QR Code should be generated."
+    article_name = "test-article"
+    assert any(
+        article_name in file.name for file in svg_files
+    ), "SVG file should be created."
